9461165e026df871347f88de34ac17ab2f852133
bimanmandal <biman29@gmail.com>
2020-09-08 08:58:41 +0530

    created stack module

---
+++ data-structures/java/stack/src/main/java/com/biman/core/ApplicationMain.java
@@ -0,0 +1,14 @@
+package com.biman.core;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApplicationMain {
+
+  static final Logger LOG = LoggerFactory.getLogger(ApplicationMain.class);
+
+  public static void main(String[] args) {
+    LOG.info("Hello World");
+  }
+}
